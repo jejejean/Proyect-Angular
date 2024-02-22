539996c988293920ea70a3c6b16e03f1cd0b9e88
jejejean <u19212532@utp.edu.pe>
2024-02-21 20:21:03 -0500

    feat: :sparkles: Added main content: header, main and footer

--- src/app/shared/components/layout/header/header.component.ts
+++ src/app/shared/components/layout/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+type Dropdown = 'planificaViaje' | 'destinosVuelos' | 'servicios';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  dropdownStates: { [key in Dropdown]: boolean } = {
+    planificaViaje: false,
+    destinosVuelos: false,
+    servicios: false,
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  toggleDropdown(dropdown: Dropdown) {
+    this.dropdownStates[dropdown] = !this.dropdownStates[dropdown];
   }
-
 }
